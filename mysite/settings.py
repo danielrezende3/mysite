836387c522083820bcf540e320868a41288a1b59
Daniel Rezende <danielrezende2@academico.ufs.br>
2023-10-24 15:26:44 -0300

    adding pythonanyhwere as allowed host

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-&iu1g!sv($xan24ve4mdz=f2ai@odo$=lrh-@37u_o7d6ifgey
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["danielrezende.pythonanywhere.com"]
 
 
 # Application definition
