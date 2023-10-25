1184563175fd3ceb11e390a1569b34755faa19b1
Daniel Rezende <danielrezende2@academico.ufs.br>
2023-10-25 16:32:29 -0300

    first try cookies

---
+++ hello/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+def sessfun(request):
+    print(request.COOKIES)
+    num_visits = request.session.get("num_visits", 0) + 1
+    request.session["num_visits"] = num_visits
+
+    if num_visits > 4:
+        del request.session["num_visits"]
+
+    resp = HttpResponse("view count=" + str(num_visits))
+    resp.set_cookie('dj4e_cookie', '41b388b1', max_age=1000)
+    return resp
